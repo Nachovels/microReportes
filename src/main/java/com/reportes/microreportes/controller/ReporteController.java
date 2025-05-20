66de6f1b1d46c74615008b6da785fc2508100eab
Jose Velasquez <velas.joseignacio@gmail.com>
2025-05-20 17:36:23 -0400

    Se realiza la conexión con microservicio de gestión de cursos con RestTemplate, se realizan métodos y se verifica la funcionalidad de estos.

--- src/main/java/com/reportes/microreportes/controller/ReporteController.java
+++ src/main/java/com/reportes/microreportes/controller/ReporteController.java
@@ -1,6 +1,9 @@
 package com.reportes.microreportes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reportes.microreportes.service.ReporteService;
@@ -10,4 +13,19 @@ public class ReporteController {
     
     @Autowired
     private ReporteService reporteService;
+
+    @GetMapping("/reporte/cursos")
+    public ResponseEntity<String> obtenerReporteCursos() {
+        return ResponseEntity.ok(reporteService.reporteCurso());
+    }
+
+    @GetMapping("/reporte/cursos/{idCurso}")
+    public ResponseEntity<String> obtenerReporteCursoPorId(@PathVariable String idCurso) {
+        return ResponseEntity.ok(reporteService.reporteCursoId(idCurso));
+    }
+
+    @GetMapping("/reporte/cursos/cantidad")
+    public ResponseEntity<String> obtenerReporteCursosCantidad() {
+        return ResponseEntity.ok(reporteService.reporteCursosCantidad());
+    }
 }
--- src/main/java/com/reportes/microreportes/service/ReporteService.java
+++ src/main/java/com/reportes/microreportes/service/ReporteService.java
@@ -2,6 +2,7 @@ package com.reportes.microreportes.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.reportes.microreportes.repository.ReporteRepository;
 
@@ -9,4 +10,30 @@ import com.reportes.microreportes.repository.ReporteRepository;
 public class ReporteService {
     @Autowired
     private ReporteRepository reporteRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String reporteCurso() {
+        String urlCurso = "http://localhost:8080/cursos";
+        String cursoData = restTemplate.getForObject(urlCurso, String.class);
+        return cursoData;
+    }
+
+    public String reporteCursoId(String idCurso) {
+        String urlCurso = "http://localhost:8080/cursos/" + idCurso;
+        String cursoData = restTemplate.getForObject(urlCurso, String.class);
+        return cursoData;
+    }
+
+    public String reporteCursosCantidad() {
+        String urlCurso = "http://localhost:8080/cursos";
+        String cursoData = restTemplate.getForObject(urlCurso, String.class);
+
+        String urlConteo ="http://localhost:8080/cursos/cantidad";
+        String cantidad = restTemplate.getForObject(urlConteo, String.class);
+
+        return "Cursos: " + cursoData + "\nCantidad de cursos: " + cantidad;
+    }
+
 }
